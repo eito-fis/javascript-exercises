2f119eb8303ab2c5f73d5ae3bdee3486a21fbb90
Eamon Ito-Fisher <eamon@itofisher.com>
2021-03-15 21:14:12 -0700

    ADD: repeatString done

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(baseStr, repeat) {
+	if (repeat < 0) {
+		return "ERROR"
+	}
+	let returnStr = "";
+	for (let i = 0; i < repeat; i++) {
+		returnStr += baseStr;
+	}
+	return returnStr
 }
 
 module.exports = repeatString
