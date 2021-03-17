2a709a908e39c1d69dd861d11a8a7b0ab5a6d8f2
Eamon Ito-Fisher <eamon@itofisher.com>
2021-03-16 19:00:56 -0700

    ADD: fibonacci done

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(idx) {
+	if (typeof idx === String) {
+		idx = parseInt(idx);
+	}
+	if (idx < 0) {
+		return "OOPS";
+	}
 
+	if (idx == 0 || idx == 1) {
+		return 1;
+	}
+	let [first, second] = [1, 1];
+	for (let i = 2; i < idx; i++) {
+		[first, second] = [second, first + second]
+	}
+	return second
 }
 
 module.exports = fibonacci
