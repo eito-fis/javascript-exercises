b8689c37d2ec33b2e1235a03fa3b23a1e94ecf2d
Eamon Ito-Fisher <eamon@itofisher.com>
2021-03-18 21:10:12 -0700

    ADD: palindromes done

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function() {
-
+const palindromes = function(palindrome) {
+	const cleanedStr = palindrome.replace(/[^a-zA-Z]/g, '').toLowerCase();
+	const revStr = cleanedStr.split("").reverse().join("");
+	if (cleanedStr == revStr) {
+		return true;
+	}
+	return false;
 }
 
 module.exports = palindromes
