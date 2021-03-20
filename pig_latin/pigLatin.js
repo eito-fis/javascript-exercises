2c5fb0e53c5663dd1848dd01608dad188f314e33
Eamon Ito-Fisher <eamon@itofisher.com>
2021-03-19 20:37:19 -0700

    ADD: pigLatin done

--- pig_latin/pigLatin.js
+++ pig_latin/pigLatin.js
@@ -1,5 +1,27 @@
-function translate() {
-	// body...
+function translateWord(word) {
+	let vowelIdx = word.search(/[aeiou]/i)
+	if (vowelIdx == -1) {
+		return "ERROR";
+	}
+
+	if (vowelIdx == 0) {
+		return word + "ay";
+	}
+	
+	// Handle "qu" special case
+	if (word[vowelIdx] == "u" && word[vowelIdx - 1] == "q") {
+		vowelIdx += 1;
+	}
+	
+	const prefix = word.slice(0, vowelIdx);
+	const suffix = word.slice(vowelIdx);
+	return suffix + prefix + "ay"
+}
+
+function translate(input) {
+	const words = input.split(" ");
+	const translated = words.map(w => translateWord(w))	;
+	return translated.join(" ");
 }
 
 
