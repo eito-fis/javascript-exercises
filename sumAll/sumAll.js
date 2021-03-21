82ab7e267b35f9574feb1152df25b3543f6cbe51
Eamon Ito-Fisher <eamon@itofisher.com>
2021-03-20 20:42:05 -0700

    ADD: sumAll Done

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+	if (typeof(a) != "number" || typeof(b) != "number") {
+		return "ERROR";
+	}
+	if (a < 0 || b < 0) {
+		return "ERROR";
+	}
 
+	let [min, max] = [null, null];
+	if (a > b) {
+		[min, max] = [b, a];
+	} else {
+		[min, max] = [a, b];
+	}
+
+	let sum = 0;
+	for (let i = min; i <= max; i++) {
+		sum += i;
+	}
+	return sum;
 }
 
 module.exports = sumAll
